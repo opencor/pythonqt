7092a6592d238724a34895470d5e7d7db2dc6f47
David Brooks <dave@bcs.co.nz>
2019-08-13 12:35:13 +1200

    addDecorators: Check for null object (commit commontk/PythonQt@ec0f11d).
    Authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- src/PythonQt.cpp
+++ src/PythonQt.cpp
@@ -1470,6 +1470,9 @@ PythonQtClassInfo* PythonQtPrivate::currentClassInfoForClassWrapperCreation()
 
 void PythonQtPrivate::addDecorators(QObject* o, int decoTypes)
 {
+  if (!o) {
+    return;
+  }
   o->setParent(this);
   int numMethods = o->metaObject()->methodCount();
   for (int i = 0; i < numMethods; i++) {
